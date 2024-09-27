435d04deb6487ad13b613f886ed642c9cc37fb32
Justin Xia <qxia3@rrc.ca>
2024-09-26 21:58:03 -0500

    debug

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,10 @@
-import adapter from '@sveltejs/adapter-node'; // 使用 Node.js 适配器
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      out: 'build', // 构建后的输出目录
-    }),
+    adapter: adapter()
   },
-  preprocess: vitePreprocess(),
+  preprocess: vitePreprocess()
 };
-
 export default config;
